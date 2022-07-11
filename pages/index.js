475e71b5727f293e24b22e2a19291f316976b2cd
gresm <78505251+gresm@users.noreply.github.com>
2022-07-11 10:40:35 +0200

    Finnaly working? :(

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,50 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import React, { Component, useState, useEffect } from 'react';
+import { React, Component } from 'react';
+
+
+function GameRunner({ story, lib }) {
+    return <script story={story} dangerouslySetInnerHTML={{ "__html": lib }}></script>
+}
+
 
 class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            story: "",
+            lib: "",
+            loaded: false
+        }
+    }
+
+    getStory = async () => {
+        fetch(window.location.origin + "/story.json")
+            .then((response) => response.text())
+            .then((text) => this.setState({ story: text }))
+            .catch((reason) => { console.log(reason) })
+    }
+
+    getLibSrc = async () => {
+        fetch(window.location.origin + "/game.js")
+            .then((response) => response.text())
+            .then((text) => this.setState({ lib: text }))
+            .catch((reason) => { console.log(reason) })
+    }
+
+    componentDidMount() {
+        if (!this.state.loaded) {
+            console.log("Reloading...")
+            this.getStory()
+            this.getLibSrc()
+        }
+    }
 
     render() {
-        const [story, setStory] = useState();
-
-        const getApiData = async () => {
-            const response = await fetch("/test.json").then(
-                (response) => response.json()
-            );
-            setStory(response);
-        };
-
-        useEffect(() => {
-            console.log(document)
-            getApiData();
-        }, []);
-
-        return <iframe className={styles.fullscreen} src='https://www.example.com' />;
+        return <div className={styles.fullscreen} id="game-div"><GameRunner story={this.state.story} lib={this.state.lib} /></div>;
     }
 }
+
 
 export default Home
