01383ea7623fab829c477794af7bd8842ea0f729
gresm <78505251+gresm@users.noreply.github.com>
2022-08-14 11:21:05 +0200

    Redirecting...

--- pages/index.js
+++ pages/index.js
@@ -3,49 +3,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { React, Component } from 'react';
 
-
-function GameRunner({ story, lib }) {
-    return (<>
-        <div dangerouslySetInnerHTML={{ "__html": lib }} />
-        <div className={styles.fullscreen} id="game-div" />
-        <div story={story} id="story" />
-    </>)
-}
-
-
 class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            story: "",
-            lib: "",
-            loaded: false
-        }
-    }
-
-    getStory = async () => {
-        fetch(window.location.origin + "/story.json")
-            .then((response) => response.text())
-            .then((text) => this.setState({ story: text }))
-            .catch((reason) => { console.log(reason) })
-    }
-
-    getLibSrc = async () => {
-        fetch(window.location.origin + "/app.html")
-            .then((response) => response.text())
-            .then((text) => this.setState({ lib: text }))
-            .catch((reason) => { console.log(reason) })
-    }
-
     componentDidMount() {
-        if (!this.state.loaded) {
-            this.getStory()
-            this.getLibSrc()
-        }
+        document.location.pathname = "/app.html"
     }
 
     render() {
-        return <div className={styles.fullscreen}><GameRunner story={this.state.story} lib={this.state.lib} /></div>;
+        return <a>Redirecting....</a>
     }
 }
 
