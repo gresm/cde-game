a5b3a916042900224fd17696796aa886a41966be
gresm <78505251+gresm@users.noreply.github.com>
2022-09-14 19:09:38 +0200

    Display with index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,111 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import { React, Component } from 'react';
 
-class Home extends Component {
-    componentDidMount() {
-        // Running on render
+
+const gameEndpoint = "/play/"
+const apiEndpoint = "/api/"
+const apiListStories = apiEndpoint + "list-stories"
+
+class StoryEntry extends Component {
+    constructor({ name, gameID, ...props }) {
+        props.name = name; props.gameID = gameID
+        super()
+        this.state = {
+            name: name,
+            gameID: gameID,
+            url: gameEndpoint + gameID
+        }
     }
 
     render() {
-        return <></>
+        return <a href={this.state.url}>{this.state.name}</a>
+    }
+}
+
+class StoriesList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            names: {},
+            loaded: false,
+            failed: false
+        }
+    }
+
+    setNames(names) {
+        this.setState({
+            names: names,
+            loaded: true,
+            failed: false
+        })
+    }
+
+    getNames() {
+        return this.state.names
+    }
+
+    fetchFailed() {
+        this.setState({
+            names: {},
+            loaded: false,
+            failed: true
+        })
+    }
+
+    didFail() {
+        return this.state.failed
+    }
+
+    didLoad() {
+        return this.state.loaded
+    }
+
+    inProcess() {
+        return !this.didLoad() && !this.didFail()
+    }
+
+    componentDidMount() {
+        fetch(apiListStories).then((value) => {
+            if (value.ok) {
+                value.json()
+                    .then((value) => { this.setNames(value) })
+                    .catch((_) => { this.fetchFailed() })
+            }
+            else {
+                this.fetchFailed()
+            }
+        }).catch((_) => { this.fetchFailed() })
+    }
+
+    render() {
+        if (this.inProcess()) {
+            return <a>Loading...</a>
+        } else if (this.didFail()) {
+            return <a>Failed to fetch data.</a>
+        }
+        else if (this.didLoad()) {
+            var games = this.getNames()
+
+            return <div>
+                {Object.keys(games).map((value, key) => {
+                    return <StoryEntry name={games[value]} key={key} gameID={value} />
+                })}
+            </div>
+        }
+        return <a>Unknown error ocurred</a>
+    }
+}
+
+class Home extends Component {
+    render() {
+        return <>
+            <Head>
+                <title>CDE game hub</title>
+            </Head>
+            <StoriesList />
+        </>
     }
 }
 
