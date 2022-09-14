c9792f638f3bce325b32d0aba8404a99c721eab4
gresm <78505251+gresm@users.noreply.github.com>
2022-09-14 20:26:58 +0200

    Simple feching api

---
+++ stories/__client.ts
@@ -0,0 +1,15 @@
+const apiRoute = "/api"
+const listStoriesRoute = apiRoute + "/list-stories"
+const getStoryRoute = apiRoute + "/story/"
+
+export function listStories() : Promise<object> {
+    return fetch(listStoriesRoute).then((value) => {
+        return value.json()
+    })
+}
+
+export function getStory(name: string) : Promise<object> {
+    return fetch(getStoryRoute + name).then((value) => {
+        return value.json()
+    })
+}
