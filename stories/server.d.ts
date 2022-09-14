2f85260df2aaf2264a6a5ab11db8dce0e3cf4278
gresm <78505251+gresm@users.noreply.github.com>
2022-09-14 13:27:09 +0200

    Make js version of server.ts

---
+++ stories/server.d.ts
@@ -0,0 +1,22 @@
+import { data } from "./stories_json"
+
+export interface StoriesData {
+    index: string[]
+    stories: {[name: string]: string}
+}
+
+export class StoriesMenager {
+    data: StoriesData
+    indexes: string[]
+    stories: {[name: string]: string}
+
+    constructor(data: StoriesData)
+
+    public collectStoryNames() : string[]
+
+    public getStory(name: string) : string
+}
+
+declare let defaultMenager: StoriesMenager
+
+export function getLoader()
---
+++ stories/server.js
@@ -0,0 +1,33 @@
+import { data } from "./stories_json"
+
+export class StoriesMenager {
+    data
+    indexes
+    stories
+
+    constructor(data) {
+        this.data = data
+        this.indexes = data.index
+        this.stories = data.stories
+    }
+
+    collectStoryNames() {
+        return this.indexes
+    }
+
+    getStory(name) {
+        if (name in this.stories){
+            return this.stories[name]
+        }
+        return ""
+    }
+}
+
+let defaultMenager
+
+export function getLoader() {
+    if (typeof defaultMenager === "undefined") {
+        defaultMenager = new StoriesMenager(data)
+    }
+    return defaultMenager
+}
