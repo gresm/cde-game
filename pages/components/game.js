1fea236027fe7fc233dc21d9357c617bd825bb9d
gresm <78505251+gresm@users.noreply.github.com>
2022-09-16 12:00:00 +0000

    Game class base.

---
+++ pages/components/game.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+
+export default class Game extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return <a>Test</a>
+    }
+}
