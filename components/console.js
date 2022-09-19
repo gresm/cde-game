37679a5b8e443f219dcb7be20a7e79b45958eaf5
gresm <78505251+gresm@users.noreply.github.com>
2022-09-19 16:09:22 +0200

    Update styles

---
+++ components/console.js
@@ -0,0 +1,18 @@
+export function ConsoleLine({ text, isInput = false }) {
+    if (isInput) {
+        return <div className="inline-div">
+            <div className='special-green-color'>cde@cde-game-web</div>
+            <div className='normal-text'>:</div>
+            <div className='special-blue-color'>~</div>
+            <div className='normal-text'>$ </div>
+            <div className='normal-text'>{text}</div>
+        </div>
+    }
+    else {
+        return <div className='special-blue-color'>{text}</div>
+    }
+}
+
+export function Cursor() {
+    return <div className='cursor'></div>
+}
