fd3c132b2c449252c3987a8a56824ca0038d5a79
gresm <78505251+gresm@users.noreply.github.com>
2022-09-27 09:03:00 +0000

    Skulpt loading script added.

--- components/game.js
+++ components/game.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { ConsoleLine, Cursor, Container } from "./console"
-
+import Script from "next/script"
 
 export class Game extends Component {
     constructor(props) {
@@ -8,13 +8,41 @@ export class Game extends Component {
         this.story = props.story
         this.name = props.name
         this.isValid = this.story !== null
+        this.mainLoaded = false
+        this.libLoaded = false
+        this.sk = null
+    }
+
+    skulptMainLoaded() {
+        this.sk = Sk
+        this.mainLoaded = true;
+        
+        if (this.mainLoaded && this.libLoaded) {
+            this.skulptLoaded();
+        }
+    }
+
+    skulptStdlibLoaded() {
+        this.libLoaded = true;
+        
+        if (this.mainLoaded && this.libLoaded) {
+            this.skulptLoaded();
+        }
+    }
+
+    skulptLoaded() {
+        
     }
 
     render() {
-        return <Container>
-            <ConsoleLine isInput={true}>./run {this.name}</ConsoleLine>
-            <ConsoleLine id='loading-screen' >{this.isValid ? "Loading..." : `Story: ${this.name} not found.`}</ConsoleLine>
-            <Cursor />
-        </Container>
+        return <>
+            <Script src='../skulpt.js' onLoad={this.skulptMainLoaded}></Script>
+            <Script src='../skulpt-stdlib.js' onLoad={this.skulptStdlibLoaded}></Script>
+            <Container>
+                <ConsoleLine isInput={true}>./run {this.name}</ConsoleLine>
+                <ConsoleLine id='loading-screen' >{this.isValid ? "Loading..." : `Story: ${this.name} not found.`}</ConsoleLine>
+                <Cursor />
+            </Container>
+        </>
     }
 }
