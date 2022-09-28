51978cab433361e4952a2d564f91c054d6735b49
gresm <78505251+gresm@users.noreply.github.com>
2022-09-28 13:47:00 +0000

    Load code from file.

--- pages/play/[game].js
+++ pages/play/[game].js
@@ -1,3 +1,4 @@
+import { readFile } from "fs/promises"
 import { getLoader } from "../../stories/server"
 import { Game } from "../../components/game"
 
@@ -7,17 +8,22 @@ import { Game } from "../../components/game"
  * @typedef { import("next").GetServerSidePropsContext } GetServerSidePropsContext
  */
 
-export default function PlayGame({ story, name }) {
-    return <Game story={story} name={name} />
+export default function PlayGame({ story, name, code }) {
+    return <Game story={story} name={name} code={code} />
 }
+
+var code = null
 
 /**
  * 
  * @param { GetServerSidePropsContext } ctx 
  * @returns 
  */
-export function getServerSideProps(ctx) {
+export async function getServerSideProps(ctx) {
+    if (code === null) {
+        code = await (await readFile("game/main.py")).toString()
+    }
     return {
-        props: {story: getLoader().getStory(ctx.query.game), name: ctx.query.game},
+        props: { story: getLoader().getStory(ctx.query.game), name: ctx.query.game, code: code}
     }
 }
