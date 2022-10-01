8d31124c3db9ddd2b756400dbe969516e488f892
gresm <78505251+gresm@users.noreply.github.com>
2022-10-01 15:26:00 +0000

    Multiple callbacks

--- utils/scriptLoader.js
+++ utils/scriptLoader.js
@@ -1,12 +1,12 @@
 // Based and hevily modified on: https://stackoverflow.com/a/15292582/15140144
 // This is shared under CC BY-SA 3.0 license (Is MIT license valid?)
 export function loadScriptsInQueue(scripts, callback) {
-    callback = callback || function () { }
+    callback = callback || [function () { }].fill(scripts.length)
     var x = 0;
     var loopArray = function (scripts) {
         // call itself
         loadScript(scripts[x], function (_script) {
-            callback(_script)
+            callback[x](_script)
             // set x to next item
             x++;
             // any more items in array?
@@ -19,14 +19,14 @@ export function loadScriptsInQueue(scripts, callback) {
 }
 
 export function loadScripts(scripts, callback) {
-    callback = callback || function () { }
+    callback = callback || [function () { }].fill(scripts.length)
 
     for (var i = 0; i < scripts.length; i++) {
-        loadScript(scripts[i], callback);
+        loadScript(scripts[i], callback[i]);
     }
 }
 
-function loadScript(src, callback) {
+export function loadScript(src, callback) {
     script = document.createElement('script');
 
     script.onerror = function () {
